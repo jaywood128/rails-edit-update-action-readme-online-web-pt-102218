3639793bd91c5481213d391eaaa1eb72fc0d5645
jaywood128 <jaywood128@gmail.com>
2019-03-29 14:41:54 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :articles, only: [:new, :index,:show, :update]
 
   put 'articles/:id', to: 'articles#update'
-  post 'articles', to: 'articles#index'
+  post 'articles', to: 'articles#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
